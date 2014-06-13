ec93ee22ea6d0bf7f070cfef8a412dfbbf1f125e
Hüseyin <hkayabilisim@gmail.com>
2014-06-14 00:53:49 +0300

    rerun of tscu_tutorial

--- src/tscu_publish.m
+++ src/tscu_publish.m
@@ -1,14 +1,5 @@
-%% Test
-% Let's try it. A side note here: as you use more and more options, you can
-% wrap the lines by using |...|. I still don't want to see the info
-% messages. So I used a proper log level. Running this command, you will 
-% obtain four figures:
-%
-% * *original signals:* The original signals are displayed. Sgsg sgsjd g. sj
-% sdg sgjjsd g sg. So I used a proper log level. Running this command, you will 
-% obtain four figures:
-% * aligned signals: The aligned signals are displayed. 
-% * warping between the time series
-% * mapping between the time series
-%publish('tscu_tutorial.m','format','pdf' ,'outputDir','../doc/pdf');
-%publish('tscu_tutorial.m','format','html','outputDir','../doc/html');
+%%
+% Publish tscu_tutorial.m in PDF and HTML format for displaying on the web
+% site.
+publish('tscu_tutorial.m','format','pdf' ,'outputDir','../doc/pdf');
+publish('tscu_tutorial.m','format','html','outputDir','../doc/html');
